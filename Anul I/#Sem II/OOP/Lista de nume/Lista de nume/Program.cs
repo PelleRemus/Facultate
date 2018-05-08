53dbc534f085b529f47134f4c69d9f972a3ead4f
PelleRemus <33091416+PelleRemus@users.noreply.github.com>
2018-05-09 00:01:57 +0300

    OOP Indexatori

---
+++ Anul I/#Sem II/OOP/Lista de nume/Lista de nume/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_de_nume
+{
+    class Nume
+    {
+        int size;
+        List<string> nume = new List<string>();
+
+        public Nume(int size, string nume)
+        {
+            this.size = size;
+            for (int i = 0; i < size; i++)
+            {
+                this.nume.Add(nume);
+            }
+        }
+
+        public string this[int index]
+        {
+            get
+            {
+                return nume[index];
+            }
+            set
+            {
+                nume[index] = value;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nume studenti = new Nume(8, "strudel");
+            studenti[2] = "Andrei";
+            Console.WriteLine(studenti[2]);
+        }
+    }
+}
