48650ea198a984c879e32c417109063f9f475c21
PelleRemus <33091416+PelleRemus@users.noreply.github.com>
2018-05-17 12:17:19 +0300

    Laborator OOP 14.05.2018

---
+++ Anul I/#Sem II/OOP/Stiva genetica/Stiva genetica/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stiva_genetica
+{
+    class Program
+    {
+        class Stiva<T>
+        {
+            int size;
+            List<T> st;
+            public Stiva(int x)
+            {
+                st = new List<T>();
+                size = x;
+            }
+            public T this[int index]
+            {
+                get
+                {
+                    return st[index];
+                }
+                set
+                {
+                    st[index] = value;
+                }
+            }
+
+            public void Push(T x)
+            {
+                if (st.Count < size)
+                    st.Add(x);
+                else
+                    Console.WriteLine("Limita de spatiu a fost atinsa");
+            }
+            public void Pop(T x)
+            {
+                if (st.Count != 0)
+                    st.Remove(x);
+                else
+                    Console.WriteLine("Nu exista elemente in stiva");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Stiva<int> st = new Stiva<int>(10);
+            st.Push(5);
+            st.Pop(3);
+        }
+    }
+}
