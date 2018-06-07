8c5b0942a068adca4b4b036205671ed8043be376
PelleRemus <33091416+PelleRemus@users.noreply.github.com>
2018-06-07 16:48:16 +0300

    O mica modificare

--- Anul I/#Sem II/Algoritmi Fundamentali/Dame/Dame/Program.cs
+++ Anul I/#Sem II/Algoritmi Fundamentali/Dame/Dame/Program.cs
@@ -16,11 +16,11 @@ namespace Dame
                 {
                     int[,] matrix = new int[n, n];
                     for (int i = 0; i < n; i++)
+                    {
                         for (int j = 0; j < n; j++)
-                        {
                             matrix[i, j] = 0;
-                            matrix[i, sol[i]] = 1;
-                        }
+                        matrix[i, sol[i]] = 1;
+                    }
 
                     for (int i = 0; i < n; i++)
                     {
