b7ae6c0d87040df8ce1822ee243505fa651cb512
PelleRemus <33091416+PelleRemus@users.noreply.github.com>
2018-06-14 21:58:34 +0300

    Mai scurt

--- Anul I/#Sem II/Geometrie/Seminar 5 - Proiect/Algoritmul lui Graham/Algoritmul lui Graham/Form1.cs
+++ Anul I/#Sem II/Geometrie/Seminar 5 - Proiect/Algoritmul lui Graham/Algoritmul lui Graham/Form1.cs
@@ -12,12 +12,10 @@ namespace Algoritmul_lui_Graham
 {
     public partial class Form1 : Form
     {
-        int[] pointsX = new int[100];
-        int[] pointsY = new int[100];
+        Point[] p = new Point[100];
         int n = 0, index;
         int[] finalPoint;
         double[] angle;
-        int[] pointName = new int[100];
         Graphics grp;
         Pen redPen = new Pen(Color.Red, 2);
         Pen whitePen = new Pen(Color.White, 2);
@@ -32,13 +30,8 @@ namespace Algoritmul_lui_Graham
 
         private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
         {
-            pointsX[n] = e.X;
-            pointsY[n] = e.Y;
-
+            p[n] = new Point(e.X, e.Y);
             grp.DrawEllipse(redPen, e.X - 1, e.Y - 1, 3, 3);
-            grp.DrawString(" " + (n + 1), new Font("Arial", 10, FontStyle.Bold), new SolidBrush(Color.DarkBlue), new Point(e.X + 5, e.Y + 5));
-
-            pointName[n] = n + 1;
             n++;
         }
 
@@ -48,22 +41,16 @@ namespace Algoritmul_lui_Graham
             Angle();
             SortAngle();
             ConvexHull();
-
-            string final = "" + pointName[0];
-            for (int i = 1; i < n; i++)
-                if (finalPoint[i] == 1)
-                    final = final + ", " + pointName[i];
-            richTextBox1.Text += "Convex Hull's Points Are \n (" + final + ")";
         }
 
         private void FindSmallestY()
         {
-            float min = pointsY[0];
+            float min = p[0].Y;
             int index = 0;
             for (int i = 1; i < n; i++)
-                if (min < pointsY[i])
+                if (min < p[i].Y)
                 {
-                    min = pointsY[i];
+                    min = p[i].Y;
                     index = i;
                 }
             this.index = index;
@@ -75,13 +62,11 @@ namespace Algoritmul_lui_Graham
             double k = 0;
             for (int i = 0; i < n; i++)
             {
-                double pointX = pointsX[index];
-                double pointY = pointsY[index];
-                if (!(pointsX[i] == pointX && pointsY[i] == pointY))
+                Point P = p[index];
+                if (!(p[i].X == P.X && p[i].Y == P.Y))
                 {
-                    double _pointX = pointsX[i] - pointX;
-                    double _pointY = pointsY[i] - pointY;
-                    k = Check(_pointX, _pointY);
+                    Point _P = new Point(p[i].X - P.X, p[i].Y - P.Y);
+                    k = Check(_P.X, _P.Y);
                 }
                 angle[i] = k;
             }
@@ -105,26 +90,17 @@ namespace Algoritmul_lui_Graham
                         angle[j - 1] = angle[j];
                         angle[j] = aux;
 
-                        int Aux = pointsX[j - 1];
-                        pointsX[j - 1] = pointsX[j];
-                        pointsX[j] = Aux;
-
-                        aux = pointsY[j - 1];
-                        pointsY[j - 1] = pointsY[j];
-                        pointsY[j] = Aux;
-
-                        Aux = pointName[j - 1];
-                        pointName[j - 1] = pointName[j];
-                        pointName[j] = Aux;
+                        Point Aux = p[j - 1];
+                        p[j - 1] = p[j];
+                        p[j] = Aux;
                     }
         }
 
         private bool ccw(int a, int b, int c)
         {
-            if (pointsX[a] * (pointsY[b] - pointsY[c]) + pointsX[b] * (pointsY[c] - pointsY[a]) + pointsX[c] * (pointsY[a] - pointsY[b]) >= 0)
+            if (p[a].X * (p[b].Y - p[c].Y) + p[b].X * (p[c].Y - p[a].Y) + p[c].X * (p[a].Y - p[b].Y) >= 0)
                 return true;
-            else
-                return false;
+            return false;
         }
 
         private void ConvexHull()
@@ -132,27 +108,21 @@ namespace Algoritmul_lui_Graham
             int a = 0, b = 1, c = 2;
             finalPoint = new int[n];
             finalPoint[a] = 1; finalPoint[b] = 1;
-            grp.DrawLine(redPen, new Point(pointsX[0], pointsY[0]), new Point(pointsX[1], pointsY[1]));
+            grp.DrawLine(redPen, p[0], p[1]);
 
             while(true)
             {
-                System.Threading.Thread.Sleep(500);
                 if (ccw(b, a, c) && c == n - 1)
                 {
                     finalPoint[c] = 1;
-                    grp.DrawLine(redPen, new Point(pointsX[b], pointsY[b]), new Point(pointsX[c], pointsX[c]));
-                    System.Threading.Thread.Sleep(500);
-                    grp.DrawLine(redPen, new Point(pointsX[c], pointsY[c]), new Point(pointsX[0], pointsY[0]));
-
-                    richTextBox1.Text += "   This is the Convex Hull\n";
+                    grp.DrawLine(redPen, p[b], p[c]);
+                    grp.DrawLine(redPen,p[c], p[0]);
                     break;
                 }
 
                 if (ccw(b, a, c))
                 {
-                    richTextBox1.Text += "LEFT TURN Between The points (" + pointName[a] + ", " + pointName[b] + ", " + pointName[c] + ")\n";
-                    grp.DrawLine(redPen, new Point(pointsX[b], pointsY[b]), new Point(pointsX[c], pointsY[c]));
-                    System.Threading.Thread.Sleep(500);
+                    grp.DrawLine(redPen, p[b], p[c]);
                     finalPoint[c] = 1;
                     a = b;
                     b = c;
@@ -161,19 +131,9 @@ namespace Algoritmul_lui_Graham
 
                 if (!ccw(b, a, c))
                 {
-                    richTextBox1.Text += "RIGHT TURN Between The points (" + pointName[a] + ", " + pointName[b] + ", " + pointName[c] + ")\n";
-                    grp.DrawLine(redPen, new Point(pointsX[b], pointsY[b]), new Point(pointsX[c], pointsY[c]));
-                    System.Threading.Thread.Sleep(500);
-
-                    grp.DrawLine(whitePen, new Point(pointsX[b], pointsY[b]), new Point(pointsX[c], pointsY[c]));
-                    grp.DrawString(" " + pointName[b], new Font("Arial", 10, FontStyle.Bold), new SolidBrush(Color.DarkBlue), new Point(pointsX[b] + 5, pointsY[b] + 5));
-
-                    grp.DrawLine(whitePen, new Point(pointsX[a], pointsY[a]), new Point(pointsX[b], pointsY[b]));
-                    grp.DrawString(" " + pointName[a], new Font("Arial", 10, FontStyle.Bold), new SolidBrush(Color.DarkBlue), new Point(pointsX[a] + 5, pointsY[a] + 5));
-
-                    grp.DrawEllipse(redPen, pointsX[b], pointsY[b], 2, 2);
-                    grp.DrawEllipse(redPen, pointsX[a], pointsY[a], 2, 2);
-                    grp.DrawEllipse(redPen, pointsX[c], pointsY[c], 2, 2);
+                    grp.DrawLine(redPen, p[b], p[c]);
+                    grp.DrawLine(whitePen, p[b], p[c]);
+                    grp.DrawLine(whitePen, p[a], p[b]);
                     finalPoint[b] = 0;
                     b = a;
                     a = theTestBefore(a);
