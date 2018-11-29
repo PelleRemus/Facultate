47f4da68e5b3a9bae24a6a97446a47e708c58b19
PelleRemus <33091416+PelleRemus@users.noreply.github.com>
2018-11-29 18:12:13 +0200

    Jocul Minesweeper

---
+++ Anul I/Sem II/Algoritmi Fundamentali/#Laborator/Minesweeper/Minesweeper/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Minesweeper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Start = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Easy = new System.Windows.Forms.Button();
+            this.Med = new System.Windows.Forms.Button();
+            this.Hard = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(900, 460);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // Start
+            // 
+            this.Start.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Start.Location = new System.Drawing.Point(906, 234);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(92, 30);
+            this.Start.TabIndex = 1;
+            this.Start.Text = "New Game";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(903, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Select Difficulty:";
+            // 
+            // Easy
+            // 
+            this.Easy.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Easy.Location = new System.Drawing.Point(906, 27);
+            this.Easy.Name = "Easy";
+            this.Easy.Size = new System.Drawing.Size(92, 30);
+            this.Easy.TabIndex = 4;
+            this.Easy.Text = "Easy";
+            this.Easy.UseVisualStyleBackColor = true;
+            this.Easy.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Easy_MouseDown);
+            // 
+            // Med
+            // 
+            this.Med.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Med.Location = new System.Drawing.Point(906, 63);
+            this.Med.Name = "Med";
+            this.Med.Size = new System.Drawing.Size(92, 30);
+            this.Med.TabIndex = 5;
+            this.Med.Text = "Medium";
+            this.Med.UseVisualStyleBackColor = true;
+            this.Med.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Med_MouseDown);
+            // 
+            // Hard
+            // 
+            this.Hard.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Hard.Location = new System.Drawing.Point(906, 99);
+            this.Hard.Name = "Hard";
+            this.Hard.Size = new System.Drawing.Size(92, 30);
+            this.Hard.TabIndex = 6;
+            this.Hard.Text = "Hard";
+            this.Hard.UseVisualStyleBackColor = true;
+            this.Hard.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Hard_MouseDown);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(906, 284);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 19);
+            this.label2.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1010, 480);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Hard);
+            this.Controls.Add(this.Med);
+            this.Controls.Add(this.Easy);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Easy;
+        private System.Windows.Forms.Button Med;
+        private System.Windows.Forms.Button Hard;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Anul I/Sem II/Algoritmi Fundamentali/#Laborator/Minesweeper/Minesweeper/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minesweeper
+{
+    public partial class Form1 : Form
+    {
+        Tile[,] matrix;
+        bool[,] b, flag;
+        int n, m, mines = 7;
+        Bitmap bmp, b1, b2, b3;
+        Graphics grp, g1, g2, g3;
+        Random r = new Random();
+        Color c = Color.FromArgb(110, 150, 210);
+
+        public Form1()
+        {
+            InitializeComponent();
+            b1 = new Bitmap(92, 30); b2 = new Bitmap(92, 30); b3 = new Bitmap(92, 30);
+            g1 = Graphics.FromImage(b1); g2 = Graphics.FromImage(b2); g3 = Graphics.FromImage(b3);
+            g1.Clear(Color.White); g2.Clear(c); g3.Clear(Color.White);
+            Easy.Image = b1; Med.Image = b2; Hard.Image = b3;
+        }
+
+        void Start_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Enabled = true;
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            grp = Graphics.FromImage(bmp);
+            matrix = new Tile[pictureBox1.Width / 20, pictureBox1.Height / 20];
+            n = matrix.GetLength(0);
+            m = matrix.GetLength(1);
+            b = new bool[n, m];
+            flag = new bool[n, m];
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++)
+                    matrix[i, j] = new Tile("0", Color.Gray);
+            int N = n * m / mines;
+            for (int k = 0; k < N; k++)
+            {
+                bool ok = true;
+                while (ok)
+                {
+                    int i = r.Next(n), j = r.Next(m);
+                    if (matrix[i, j].x == "0")
+                    {
+                        matrix[i, j].x = "X";
+                        ok = false;
+                    }
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++)
+                {
+                    int x = 0;
+                    if (matrix[i, j].x == "X") continue;
+                    for (int k = i - 1; k <= i + 1; k++)
+                        for (int l = j - 1; l <= j + 1; l++)
+                            if (k >= 0 && k < n && l >= 0 && l < m)
+                                if (matrix[k, l].x == "X") x++;
+                    matrix[i, j].x = x.ToString();
+                    switch (x)
+                    {
+                        case 1: matrix[i, j].color = Color.FromArgb(20, 100, 200); break;
+                        case 2: matrix[i, j].color = Color.Green; break;
+                        case 3: matrix[i, j].color = Color.Red; break;
+                        case 4: matrix[i, j].color = Color.DarkBlue; break;
+                        case 5: matrix[i, j].color = Color.DarkRed; break;
+                        case 6: matrix[i, j].color = Color.DarkTurquoise; break;
+                        case 7: matrix[i, j].color = Color.Black; break;
+                        default: break;
+                    }
+                }
+
+            grp.Clear(Color.LightGray);
+            for (int i = 1; i < n; i++)
+            {
+                int x1 = i * 20, x2, y1 = 0, y2 = pictureBox1.Height;
+                grp.DrawLine(Pens.Black, x1, y1, x1, y2);
+                x1 = 0; x2 = pictureBox1.Width; y1 = i * 20;
+                grp.DrawLine(Pens.Black, x1, y1, x2, y1);
+            }
+            pictureBox1.Image = bmp;
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int i = e.X / 20, j = e.Y / 20;
+            if (e.Button==MouseButtons.Left)
+            {
+                if (!flag[i,j])
+                {
+                    if (matrix[i, j].x == "X")
+                        GameOver();
+                    else
+                        Parcurgere(i, j);
+                }
+            }
+            else
+            {
+                if (!b[i, j])
+                {
+                    if (flag[i,j])
+                    {
+                        grp.FillRectangle(new SolidBrush(Color.LightGray), i * 20, j * 20, 20, 20);
+                        flag[i, j] = false;
+                    }
+                    else
+                    {
+                        grp.FillRectangle(new SolidBrush(Color.Red), i * 20, j * 20, 20, 20);
+                        flag[i,j] = true;
+                    }
+                }
+            }
+
+            bool ok = true;
+            for(int k=0; k<n; k++)
+                for(int l=0; l<m; l++)
+                {
+                    if (matrix[k, l].x == "X")
+                        if (!flag[k, l])
+                            ok = false;
+                    if (flag[k, l])
+                        if (matrix[k, l].x != "X")
+                            ok = false;
+                }
+            pictureBox1.Image = bmp;
+            if (ok)
+            {
+                MessageBox.Show("You Win!");
+                pictureBox1.Enabled = false;
+            }
+        }
+
+        void GameOver()
+        {
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++)
+                {
+                    if (matrix[i, j].x == "0") continue;
+                    SolidBrush brush = new SolidBrush(matrix[i,j].color);
+                    grp.DrawString(matrix[i, j].x, new Font("Arial", 15, FontStyle.Bold), brush, new Point(i * 20, j * 20));
+                }
+            pictureBox1.Image = bmp;
+            pictureBox1.Enabled = false;
+            MessageBox.Show("You Lose!");
+        }
+
+        void Parcurgere(int i, int j)
+        {
+            if (i >= 0 && i < n && j >= 0 && j < m && b[i, j] == false)
+            {
+                b[i, j] = true;
+                grp.FillRectangle(new SolidBrush(Color.White), i * 20, j * 20, 20, 20);
+                if (matrix[i, j].x == "0")
+                {
+                    Parcurgere(i - 1, j);
+                    Parcurgere(i, j + 1);
+                    Parcurgere(i + 1, j);
+                    Parcurgere(i, j - 1);
+                }
+                else
+                    grp.DrawString(matrix[i, j].x, new Font("Arial", 15, FontStyle.Bold), new SolidBrush(matrix[i, j].color), new Point(i * 20, j * 20));
+            }
+        }
+
+        private void Easy_MouseDown(object sender, MouseEventArgs e)
+        {
+            g1.Clear(c);
+            g2.Clear(Color.White);
+            g3.Clear(Color.White);
+            mines = 10;
+            Easy.Image = b1; Med.Image = b2; Hard.Image = b3;
+        }
+
+        private void Med_MouseDown(object sender, MouseEventArgs e)
+        {
+            g1.Clear(Color.White);
+            g2.Clear(c);
+            g3.Clear(Color.White);
+            mines = 7;
+            Easy.Image = b1; Med.Image = b2; Hard.Image = b3;
+        }
+
+        private void Hard_MouseDown(object sender, MouseEventArgs e)
+        {
+            g1.Clear(Color.White);
+            g2.Clear(Color.White);
+            g3.Clear(c);
+            mines = 5;
+            Easy.Image = b1; Med.Image = b2; Hard.Image = b3;
+        }
+    }
+
+    public class Tile
+    {
+        public string x;
+        public Color color;
+        public Tile(string x, Color c)
+        {
+            this.x = x;
+            color = c;
+        }
+    }
+}
