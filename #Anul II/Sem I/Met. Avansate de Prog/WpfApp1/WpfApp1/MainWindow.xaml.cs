75266786e8ebeea65f3cc3e3e2ad4b26e2e9da1d
PelleRemus <33091416+PelleRemus@users.noreply.github.com>
2018-12-17 10:22:11 +0200

    Curs MAP 17.12.2018 - Model pentru tema de la criptografie, folosind WPF

---
+++ #Anul II/Sem I/Met. Avansate de Prog/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainWindowOnLoad();
+        }
+
+        string cript, key, decript;
+        int n, m;
+        char[,] matrix;
+        int[] permutare;
+
+        private void MainWindowOnLoad()
+        {
+            TextReader dataLoad = new StreamReader(@"../../TextFile1.txt");
+
+            string buffer = dataLoad.ReadLine();
+            txb_msgCript.Text = buffer;
+            cript = buffer;
+
+            buffer = dataLoad.ReadLine();
+            txb_key.Text = buffer;
+            key = buffer;
+
+            m = key.Length;
+            n = cript.Length / m;
+
+            matrix = new char[n, m];
+
+            int k = 0;
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++)
+                {
+                    matrix[i, j] = cript[k];
+                    k++;
+                }
+        }
+
+        private void Decript_Click(object sender, RoutedEventArgs e)
+        {
+            //am implementat diferit de cum a explicat profu
+            //si nu stiu daca merge bine
+            permutare = new int[m];
+            int k = 0;
+            for (int i = 0; i < 26; i++)
+            {
+                char litera = (char)('a' + i);
+                for (int j = 0; j <= n; j++)
+                {
+                    if (key[j] == litera)
+                    {
+                        permutare[k] = 'z' - litera;
+                        k++;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
