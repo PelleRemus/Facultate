7353b028fd10711825835e922cd4229f415bf96a
Nicholas <remusnicolaepelle@gmail.com>
2019-01-24 02:55:19 +0200

    Laborator MAP 19.01.2019 - Exercitii folosind clasa Lista creata de utilizator

---
+++ #Anul II/Sem I/Met. Avansate de Prog/#Laborator/Lista/Lista/Lista.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista
+{
+    public class Lista
+    {
+        public int n;
+        public int[] v;
+
+        public Lista()
+        {
+            n = 0;
+            v = new int[0];
+        }
+
+        public void Add(int value)
+        {
+            n++;
+            int[] newV = new int[n];
+            for (int i = 0; i < n - 1; i++)
+                newV[i] = v[i];
+            newV[n - 1] = value;
+            v = newV;
+        }
+
+        public void Permutare(int k)
+        {
+            if (k > n)
+                k = n % k;
+            int[] aux = new int[n];
+
+            for(int i=0; i<n; i++)
+            {
+                if (i - k < 0)
+                    aux[n + i - k] = v[i];
+                else
+                    aux[i - k] = v[i];
+            }
+
+            v = aux;
+        }
+
+        public void Afisare()
+        {
+            for (int i = 0; i < n; i++)
+                Console.Write(v[i] + " ");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ #Anul II/Sem I/Met. Avansate de Prog/#Laborator/Lista/Lista/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextReader dataload = new StreamReader(@"../../TextFile1.txt");
+            int k = int.Parse(dataload.ReadLine());
+            string buffer = dataload.ReadLine();
+            string[] s = buffer.Split(' ');
+
+            Lista lista = new Lista();
+            Lista listaPara = new Lista();
+            Lista listaImpara = new Lista();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                lista.Add(int.Parse(s[i]));
+                if (lista.v[i] % 2 == 0)
+                    listaPara.Add(lista.v[i]);
+                else
+                    listaImpara.Add(lista.v[i]);
+            }
+
+            Console.WriteLine("Exercitiul 1:");
+            lista.Afisare();
+            lista.Permutare(k);
+            lista.Afisare();
+            Console.ReadKey();
+
+            Console.WriteLine();
+            Console.WriteLine("Exercitiul 2:");
+            listaPara.Afisare();
+            listaImpara.Afisare();
+            Console.ReadKey();
+        }
+    }
+}
