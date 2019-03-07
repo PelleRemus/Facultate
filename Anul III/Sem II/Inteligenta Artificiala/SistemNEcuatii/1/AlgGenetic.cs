78b2c5877af16dbb39974a2e3fb23d46826cb0d7
Nicholas <remusnicolaepelle@gmail.com>
2019-03-07 23:16:12 +0200

    Curs AI 07.03.2019 - Rezolvarea unui sistem de n ecuatii cu n necunoscute folosind algoritmi genetici si operatori de selectie, mutatie si incrucisare

---
+++ Anul III/Sem II/Inteligenta Artificiala/SistemNEcuatii/1/AlgGenetic.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    public static class AlgGenetic
+    {
+        public static Cromozom[] cromozomi;
+        public static Cromozom[] parinti;
+        public static int N;
+        public static int K;
+
+        public static void initPop(int n, int k)
+        {
+            N = n;
+            K = k;
+            cromozomi = new Cromozom[N];
+            parinti = new Cromozom[K];
+            for (int i=0; i<N; i++)
+                cromozomi[i] = new Cromozom();
+        }
+
+        public static void Selectie()
+        {
+            Ordonare();
+            for (int i = 0; i < K; i++)
+                parinti[i] = cromozomi[i];
+        }
+
+        public static void Ordonare()
+        {
+            for (int i = 0; i < N - 1; i++)
+                for (int j = i + 1; j < N; j++)
+                    if (cromozomi[i].FAdec() > cromozomi[j].FAdec())
+                    {
+                        Cromozom c = cromozomi[i];
+                        cromozomi[i] = cromozomi[j];
+                        cromozomi[j] = c;
+                    }
+        }
+
+        public static Cromozom Incrucisare()
+        {
+            int i, j;
+            do
+            {
+                i = Program.rnd.Next(K);
+                j = Program.rnd.Next(K);
+            } while (i == j);
+            Cromozom c = new Cromozom();
+            c.gene[0] = parinti[i].gene[0];
+            c.gene[1] = parinti[i].gene[1];
+            c.gene[2] = parinti[j].gene[2];
+            c.gene[3] = parinti[j].gene[3];
+            c.gene[4] = parinti[j].gene[4];
+            return c;
+        }
+
+        public static void UpdatePop()
+        {
+            for(int i=0; i<N; i++)
+            {
+                Cromozom temp = Incrucisare();
+                temp.Mutatie();
+                cromozomi[i] = temp;
+            }
+        }
+
+        public static void View()
+        {
+            for (int i = 0; i < N; i++)
+                cromozomi[i].View();
+        }
+
+        public static void ViewSol()
+        {
+            for (int i = 0; i < K; i++)
+                parinti[i].View();
+        }
+    }
+}
---
+++ Anul III/Sem II/Inteligenta Artificiala/SistemNEcuatii/1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _1
+{
+    class Program
+    {
+        public static float[,] A;
+        public static float[] T;
+        public static int n;
+        public static Random rnd = new Random();
+
+        static void Main(string[] args)
+        {
+            Citire();
+            Afisare();
+
+            AlgGenetic.initPop(200, 10);
+            int i = 0;
+            do
+            {
+                i++;
+                AlgGenetic.Selectie();
+                AlgGenetic.UpdatePop();
+
+                if (i == 1000)
+                {
+                    AlgGenetic.ViewSol();
+                    Console.WriteLine();
+                    i = 0;
+                }
+            } while (true);
+        }
+
+        static void Citire()
+        {
+            TextReader dataLoad = new StreamReader(@"../../Matrice.txt");
+            n = int.Parse(dataLoad.ReadLine());
+            A = new float[n, n];
+            T = new float[n];
+            string buffer;
+            string[] x;
+
+            for (int i=0; i<n; i++)
+            {
+                buffer = dataLoad.ReadLine();
+                x = buffer.Split(' ');
+                for (int j = 0; j < n; j++)
+                    A[i, j] = float.Parse(x[j]);
+            }
+
+            buffer = dataLoad.ReadLine();
+            x = buffer.Split(' ');
+            for (int i = 0; i < n; i++)
+                T[i] = float.Parse(x[i]);
+        }
+
+        static void Afisare()
+        {
+            for(int i=0; i<n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                    Console.Write(A[i, j] + "*x" + j + "\t");
+                Console.WriteLine("=" + T[i]);
+            }
+        }
+    }
+}
