c41d2e0a94a1176867e81d01784bcd2c577e8414
Nicholas <remusnicolaepelle@gmail.com>
2019-03-14 22:50:27 +0200

    Tema #2 - Crearea unei aplicatii web folosind c# si modelul MVC, avand la baza un model de film si un controller cu actiunile CRUD.

---
+++ #Anul II/Sem II/Dezvoltarea Aplicatiilor Web/Movies/Movies/Models/Movie.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Movies.Models
+{
+    public class Movie
+    {
+        public Guid Id { get; set; }
+
+        public List<string> Actors;
+
+        public List<string> Genres;
+
+        public string Title { get; set; }
+
+        public int PG { get; set; }
+
+        public int Awards { get; set; }
+
+        public string Director { get; set; }
+
+        public int Duration { get; set; }
+
+        public int ReleaseYear { get; set; }
+
+        public Movie(List<string> Actors, List<string> Genres, string Title, int PG = 13, 
+            int Awards = 0, string Director = "Vlad", int Duration = 120, int ReleaseYear = 2000)
+        {
+            this.Id = Guid.NewGuid();
+            this.Actors = Actors;
+            this.Genres = Genres;
+            this.Title = Title;
+            this.PG = PG;
+            this.Awards = Awards;
+            this.Director = Director;
+            this.Duration = Duration;
+            this.ReleaseYear = ReleaseYear;
+        }
+    }
+}
