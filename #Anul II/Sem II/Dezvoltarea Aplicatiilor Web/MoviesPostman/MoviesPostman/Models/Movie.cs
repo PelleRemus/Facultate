12f3f7a5d55345d3a456bd1c2dc84ff73323df90
PelleRemus <33091416+PelleRemus@users.noreply.github.com>
2019-03-27 13:03:18 +0200

    Tema #3 - Crearea unei aplicatii folosind ruta si API. Verificam functionalitatea in Postman.

---
+++ #Anul II/Sem II/Dezvoltarea Aplicatiilor Web/MoviesPostman/MoviesPostman/Models/Movie.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace MoviesPostman.Models
+{
+    public class Movie
+    {
+        public Guid Id { get; set; }
+
+        public string[] Actors;
+
+        public string[] Genres;
+
+        [Required(ErrorMessage = "Please insert a title for your movie")]
+        public string Title { get; set; }
+
+        public int PG { get; set; }
+
+        public int Awards { get; set; }
+
+        [Required(ErrorMessage = "A movie must have a director")]
+        public string Director { get; set; }
+
+        public int Duration { get; set; }
+
+        public int ReleaseYear { get; set; }
+
+        public void Clone(Movie model)
+        {
+            PropertyInfo[] properties = model.GetType().GetProperties();
+
+            for (int i = 0; i < properties.Length; i++)
+            {
+                properties[i].SetValue(this, properties[i].GetValue(model));
+            }
+        }
+    }
+}
