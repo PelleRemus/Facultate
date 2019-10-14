2b9a688a80bb5e7c8c3e795b8b78c15ef6cd2dab
PelleRemus <remusnicolaepelle@gmail.com>
2019-10-14 18:38:30 +0300

    Laborator Calcul Numeric 14.10.2019 - Metoda Eliminarii Gauss functioneaza cum trebuie acum. Prima incercare a Algoritmului lui Iacobi

---
+++ Anul III/Sem I 2019-2020/Calcul Numeric/Laborator 1/MetEliminariiGauss/MetEliminariiGauss/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace MetEliminariiGauss
+{
+    class Program
+    {
+        static int n;
+        static double[,] a;
+        static double[] x, b;
+
+        static void Main(string[] args)
+        {
+            n = 3;
+            a = new double[,] {
+                { 10, 2, -1 },
+                { -2, -5, 1 },
+                { -3, 1, -5 }
+            };
+            b = new double[] { 9, -1, -8 };
+            x = new double[n];
+
+            AfisareMatrice();
+            for (int k = 0; k < n - 1; k++)
+            {
+                if (a[k, k] != 0)
+                {
+                    double p = a[k, k];
+                    for (int j = k; j < n; j++)
+                        a[k, j] /= p;
+                    b[k] /= p;
+
+                    for (int i = k + 1; i < n; i++)
+                    {
+                        for (int j = k + 1; j < n; j++)
+                            a[i, j] -= a[k, j] * a[i, k];
+                        b[i] -= b[k] * a[i, k];
+                        a[i, k] -= a[k, k] * a[i, k];
+                    }
+                    AfisareMatrice();
+                }
+                else
+                {
+                    Console.WriteLine("Can not divide by 0.");
+                    Console.ReadKey();
+                    return;
+                }
+            }
+            SistSuperiorTriunghiular();
+            Afisare();
+            Console.ReadKey();
+        }
+
+        static void SistSuperiorTriunghiular()
+        {
+            for (int k = n - 1; k >= 0; k--)
+            {
+                double s = 0;
+                for (int i = k + 1; i < n; i++)
+                    s += a[k, i] * x[i];
+                x[k] = (b[k] - s) / a[k, k];
+            }
+        }
+
+        static void Afisare()
+        {
+            for (int i = 0; i < n; i++)
+                Console.Write(x[i] + " ");
+            Console.WriteLine();
+        }
+
+        static void AfisareMatrice()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                    Console.Write(a[i, j] + "\t");
+                Console.WriteLine("= " + b[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Anul III/Sem I 2019-2020/Calcul Numeric/Laborator 2/AlgoritmIacobi/AlgoritmIacobi/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmIacobi
+{
+    class Program
+    {
+        static int n, k;
+        static double epsilon;
+        static double[,] a, alpha;
+        static double[] b, beta;
+        static List<double[]> x = new List<double[]>();
+
+        static void Main(string[] args)
+        {
+            //initializare
+            n = 3; k = 0;
+            a = new double[,] {
+                { 10, 2, -1 },
+                { -2, -5, 1 },
+                { -3, 1, -5 }
+            };
+            b = new double[] { 9, -1, -8 };
+            epsilon = 0.00001;
+            alpha = new double[n, n];
+            beta = new double[n];
+            x.Add(new double[n]);
+
+            //calculare alpha si beta
+            for (int i = 0; i < n; i++)
+            {
+                if (a[i, i] != 0)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (j == i)
+                            continue;
+                        alpha[i, j] = -a[i, j] / a[i, i];
+                    }
+                    beta[i] = b[i] / a[i, i];
+                }
+                else
+                {
+                    Console.WriteLine("Can not divide by 0.");
+                    Console.ReadKey();
+                    return;
+                }
+            }
+
+            //algoritm
+            for (int i = 0; i < n; i++)
+                x[k][i] = beta[i];
+            do
+            {
+                k++;
+                x.Add(new double[n]);
+                for (int i = 0; i < n; i++)
+                {
+                    double s = 0;
+                    for (int j = 0; j < n; j++)
+                        s += alpha[i, j] * x[k - 1][j];
+                    x[k][i] = (beta[i] - s) / a[i, i];
+                }
+            } while (MaxError() >= epsilon);
+
+            Console.WriteLine(k);
+            Afisare();
+            Console.ReadKey();
+        }
+
+        static double MaxError()
+        {
+            double max = 0;
+            for (int i = 0; i < n; i++)
+            {
+                double aux = Math.Abs(x[k][i] - x[k - 1][i]);
+                if (aux > max)
+                    max = aux;
+            }
+            return max;
+        }
+
+        static void Afisare()
+        {
+            for (int i = 0; i < n; i++)
+                Console.Write(x[k][i] + " ");
+            Console.WriteLine();
+        }
+    }
+}
