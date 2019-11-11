bb175dc5812489b1bf6a7ee7e0a8bf85e565fb90
PelleRemus <remusnicolaepelle@gmail.com>
2019-11-11 19:24:19 +0200

    Updated Metoda Tangentei

--- Anul III/Sem I 2019-2020/Calcul Numeric/Laborator 5/MetodaTangentei/MetodaTangentei/Program.cs
+++ Anul III/Sem I 2019-2020/Calcul Numeric/Laborator 5/MetodaTangentei/MetodaTangentei/Program.cs
@@ -13,35 +13,40 @@ namespace MetodaTangentei
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Determinati intervalele [a, b] ale solutiilor unei ecuatii, si introduceti capetele unui interval de la tastatura:");
-            /*Console.Write("a = ");
+            /*Console.WriteLine("Determinati intervalele [a, b] ale solutiilor unei ecuatii, si introduceti capetele unui interval de la tastatura:");
+            Console.Write("a = ");
             x = double.Parse(Console.ReadLine());
             Console.Write("b = ");
             y = double.Parse(Console.ReadLine());*/
-            epsilon = 10e-8;
+            epsilon = 1e-10;
 
-            m = 2; c = 3;
-            x = 1; y = c;
+            /*m = 3; c = 3;
+            Console.WriteLine("Radicalul de ordinul {0} din {1} este:", m, c);
+            x = 1; y = c;*/
+            x = 0.25 * Math.PI; y = 0.5 * Math.PI;
 
             k = 0;
-            dda = m * (m - 1);
+            //dda = m * (m - 1);
+            dda = 0.25 * Math.Sqrt(2);
             
             MetodaTangentei();
 
-            Console.WriteLine("Solutia este {0} si a fost determinata dupa {1} iteratii.", xk, k);
+            //Console.WriteLine("Solutia este {0} si a fost determinata dupa {1} iteratii.", xk, k);
+            Console.WriteLine("{0} (determinat dupa {1} iteratii)", xk, k);
             Console.ReadKey();
         }
 
         private static void MetodaTangentei()
         {
-            if (f(x, m, c) * dda < 0)
+            if (kepler(x) * dda < 0)
                 x = y;
-            xk = c;
+            xk = x;
             do
             {
+                Console.WriteLine(x + " " + xk);
                 k++;
                 x = xk;
-                xk = x - f(x, m, c) / df(x, m);
+                xk = x - kepler(x) / keplerDerivat(x);
             } while (Math.Abs(xk - x) > epsilon);
         }
         
@@ -53,6 +58,16 @@ namespace MetodaTangentei
         static double df(double x, int m)
         {
             return m * Math.Pow(x, m - 1);
+        }
+
+        static double kepler(double x)
+        {
+            return x - 0.5 * Math.Sin(x) - 0.25;
+        }
+
+        static double keplerDerivat(double x)
+        {
+            return 1 - 0.5 * Math.Cos(x);
         }
     }
 }
