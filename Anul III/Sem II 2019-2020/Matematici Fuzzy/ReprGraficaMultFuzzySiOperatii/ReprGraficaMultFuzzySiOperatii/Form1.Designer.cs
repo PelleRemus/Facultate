8c8908ad4920a63dcfc3fbc3c718a2e192705351
PelleRemus <remusnicolaepelle@gmail.com>
2020-03-03 01:34:30 +0200

    Seminar MF 02.03.2020 - Reprezentarea grafica a a multimilor fuzzy si a operatiilor pe acestea

---
+++ Anul III/Sem II 2019-2020/Matematici Fuzzy/ReprGraficaMultFuzzySiOperatii/ReprGraficaMultFuzzySiOperatii/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace ReprGraficaMultFuzzySiOperatii
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(902, 570);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(908, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "A si B";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(908, 54);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(139, 36);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "A1/2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.Location = new System.Drawing.Point(908, 96);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(139, 36);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "A1 si B1";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button4.Location = new System.Drawing.Point(908, 138);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(139, 52);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "suppA si suppB";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button5.Location = new System.Drawing.Point(908, 196);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(139, 36);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "A∨B si A∧B";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button7.Location = new System.Drawing.Point(908, 238);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(139, 36);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "1-A";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button8.Location = new System.Drawing.Point(908, 280);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(139, 36);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "1-B";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1059, 582);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ Anul III/Sem II 2019-2020/Matematici Fuzzy/ReprGraficaMultFuzzySiOperatii/ReprGraficaMultFuzzySiOperatii/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ReprGraficaMultFuzzySiOperatii
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int width, height;
+        float h = 0.001f, scale = 11;
+        Graphics grp;
+        Bitmap bmp;
+        Point center;
+
+        void DrawSketch()
+        {
+            width = pictureBox1.Width - 20;
+            height = pictureBox1.Height - 20;
+
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            grp = Graphics.FromImage(bmp);
+            grp.DrawLine(Pens.Black, 0, height, pictureBox1.Width, height);
+            grp.DrawLine(Pens.Black, 20, 0, 20, pictureBox1.Height);
+
+            grp.DrawString("0", new Font("Arial", 15), new SolidBrush(Color.Black), 0, height - 15);
+            grp.DrawString("1", new Font("Arial", 15), new SolidBrush(Color.Black), 0, 0);
+            grp.DrawString("0", new Font("Arial", 15), new SolidBrush(Color.Black), 20, height);
+            grp.DrawString(scale.ToString(), new Font("Arial", 15), new SolidBrush(Color.Black), width - 15, height);
+            
+            center = new Point(20, height);
+            pictureBox1.Image = bmp;
+        }
+
+        void SetPoint(float n, Color c)
+        {
+            grp.DrawString(n.ToString(), new Font("Arial", 15), new SolidBrush(c), 10 + n * width / scale, height);
+        }
+        void Legend(string s, int i, Color c)
+        {
+            grp.FillRectangle(new SolidBrush(c), 30, 10 + i * 25, 15, 15);
+            grp.DrawString(s, new Font("Arial", 15), new SolidBrush(Color.Black), 45, 5 + i * 25);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DrawSketch();
+            SetPoint(1, Color.Blue);
+            SetPoint(2, Color.Red);
+            SetPoint(3, Color.Red);
+            SetPoint(4, Color.Red);
+            SetPoint(6, Color.Red);
+            SetPoint(7, Color.Blue);
+            SetPoint(10, Color.Blue);
+            Legend("A", 0, Color.Blue);
+            Legend("B", 1, Color.Red);
+
+            for (int k=0; k<11000; k++)
+            {
+                float x = center.X + k * h * width / scale - 1;
+                float y = center.Y - A(k * h) * height - 3;
+                grp.FillEllipse(new SolidBrush(Color.Blue), x, y, 3, 3);
+
+                y = center.Y - B(k * h) * height;
+                grp.FillEllipse(new SolidBrush(Color.Red), x, y, 3, 3);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DrawSketch();
+            grp.DrawString("1/2", new Font("Arial", 15), new SolidBrush(Color.Blue), 20, height / 2);
+            SetPoint(4, Color.Blue);
+            SetPoint(7, Color.Blue);
+            SetPoint(8.5f, Color.Blue);
+            Legend("A", 0, Color.LightGray);
+            Legend("A1/2", 1, Color.Blue);
+
+            for (int k = 0; k < 11000; k++)
+            {
+                float x = center.X + k * h * width / scale - 1;
+                float y = center.Y - A(k * h) * height - 1;
+
+                Color c = Color.LightGray;
+                if (A(k * h) >= 0.5)
+                    c = Color.Blue;
+                grp.FillEllipse(new SolidBrush(c), x, y, 3, 3);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DrawSketch();
+            SetPoint(3, Color.Red);
+            SetPoint(4, Color.Red);
+            SetPoint(7, Color.Blue);
+            Legend("A, B", 0, Color.LightGray);
+            Legend("A1", 1, Color.Blue);
+            Legend("B1", 2, Color.Red);
+
+            for (int k = 0; k < 11000; k++)
+            {
+                float x = center.X + k * h * width / scale - 1;
+                float y = center.Y - A(k * h) * height - 1;
+                grp.FillEllipse(new SolidBrush(Color.LightGray), x, y, 3, 3);
+
+                y = center.Y - B(k * h) * height - 1;
+                grp.FillEllipse(new SolidBrush(Color.LightGray), x, y, 3, 3);
+            }
+
+            for (int k = 0; k < 11000; k++)
+            {
+                float x = center.X + k * h * width / scale - 2;
+                float y = center.Y - A(k * h) * height - 2;
+                if (k == 7000)
+                    grp.FillEllipse(new SolidBrush(Color.Blue), x, y, 5, 5);
+
+                y = center.Y - B(k * h) * height - 2;
+                if (B(k * h) == 1)
+                    grp.FillEllipse(new SolidBrush(Color.Red), x, y, 5, 5);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DrawSketch();
+            SetPoint(1, Color.Blue);
+            SetPoint(2, Color.Red);
+            SetPoint(3, Color.Red);
+            SetPoint(4, Color.Red);
+            SetPoint(6, Color.Red);
+            SetPoint(7, Color.Blue);
+            SetPoint(10, Color.Blue);
+            Legend("suppA", 0, Color.Blue);
+            Legend("suppB", 1, Color.Red);
+
+            for (int k = 0; k < 11000; k++)
+            {
+                float x = center.X + k * h * width / scale - 1;
+                float y = center.Y - A(k * h) * height - 1;
+                if (A(k * h) > 0)
+                    grp.FillEllipse(new SolidBrush(Color.Blue), x, y, 3, 3);
+
+                y = center.Y - B(k * h) * height - 1;
+                if (B(k * h) > 0)
+                    grp.FillEllipse(new SolidBrush(Color.Red), x, y, 3, 3);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            DrawSketch();
+            SetPoint(2.2f, Color.Red);
+            SetPoint(4.75f, Color.Blue);
+            Legend("A∨B", 0, Color.Blue);
+            Legend("A∧B", 1, Color.Red);
+
+            for (int k = 0; k < 11000; k++)
+            {
+                float x = center.X + k * h * width / scale - 1;
+                float y = center.Y - Math.Max(A(k * h), B(k * h)) * height - 1;
+                grp.FillEllipse(new SolidBrush(Color.Blue), x, y, 3, 3);
+
+                y = center.Y - Math.Min(A(k * h), B(k * h)) * height + 1;
+                grp.FillEllipse(new SolidBrush(Color.Red), x, y, 3, 3);
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            DrawSketch();
+            grp.DrawLine(new Pen(Color.Green, 2), 0, height / 2, pictureBox1.Width, height / 2);
+            grp.DrawString("1/2", new Font("Arial", 15), new SolidBrush(Color.Green), 20, height / 2);
+            SetPoint(1, Color.Blue);
+            SetPoint(4, Color.Green);
+            SetPoint(7, Color.Red);
+            SetPoint(8.5f, Color.Green);
+            SetPoint(10, Color.Blue);
+            Legend("A", 0, Color.Blue);
+            Legend("1-A", 1, Color.Red);
+
+            for (int k = 0; k < 11000; k++)
+            {
+                float x = center.X + k * h * width / scale - 1;
+                float y = center.Y - A(k * h) * height - 1;
+                grp.FillEllipse(new SolidBrush(Color.Blue), x, y, 3, 3);
+
+                y = center.Y - (1 - A(k * h)) * height - 1;
+                grp.FillEllipse(new SolidBrush(Color.Red), x, y, 3, 3);
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            scale = 7;
+            DrawSketch();
+            grp.DrawLine(new Pen(Color.Green, 2), 0, height / 2, pictureBox1.Width, height / 2);
+            grp.DrawString("1/2", new Font("Arial", 15), new SolidBrush(Color.Green), 20, height / 2);
+            SetPoint(2, Color.Blue);
+            SetPoint(2.5f, Color.Green);
+            SetPoint(3, Color.Red);
+            SetPoint(4, Color.Red);
+            SetPoint(5, Color.Green);
+            SetPoint(6, Color.Blue);
+            Legend("B", 0, Color.Blue);
+            Legend("1-B", 1, Color.Red);
+
+            for (int k = 0; k < 11000; k++)
+            {
+                float x = center.X + k * h * width / scale - 1;
+                float y = center.Y - B(k * h) * height - 1;
+                grp.FillEllipse(new SolidBrush(Color.Blue), x, y, 3, 3);
+
+                y = center.Y - (1 - B(k * h)) * height - 1;
+                grp.FillEllipse(new SolidBrush(Color.Red), x, y, 3, 3);
+            }
+            scale = 11;
+        }
+
+        float A(float x)
+        {
+            if (x < 1) return 0;
+            if (x < 7) return (x - 1) / 6;
+            if (x < 10) return (10 - x) / 3;
+            return 0;
+        }
+
+        float B(float x)
+        {
+            if (x < 2) return 0;
+            if (x < 3) return x - 2;
+            if (x < 4) return 1;
+            if (x < 6) return (6 - x) / 2;
+            return 0;
+        }
+    }
+}
