f58981e4db8c41bb7107441378304aca67447ad7
PelleRemus <remusnicolaepelle@gmail.com>
2020-03-05 17:19:08 +0200

    Curs TAP 05.03.2020 - Inceput de aplicatie, cu planete care sunt conectate ca si un graf. Luam datele din fisierul text demo_map.map. Inceput de documentatie (partea de word cu 40 de pagini), cu scrisul formatat corect. O imagine pusa in Word.

---
+++ Anul III/Sem II 2019-2020/Tehnici avansate de programare/LicentaDemo/LicentaDemo/Engine.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace LicentaDemo
+{
+    public static class Engine
+    {
+        public static List<Planet> planets = new List<Planet>();
+        public static int[,] ma;
+        public static int n;
+        public static float zoomX = 1, zoomY = 1;
+
+        public static Color backColor = Color.FromArgb(20, 20, 50);
+        public static PictureBox display;
+        public static Graphics grp;
+        public static Bitmap bmp;
+
+        public static void InitGraph(PictureBox p)
+        {
+            display = p;
+            bmp = new Bitmap(p.Width, p.Height);
+            grp = Graphics.FromImage(bmp);
+            grp.Clear(backColor);
+
+            display.Image = bmp;
+        }
+
+        public static void Load(string fileName)
+        {
+            TextReader dataLoad = new StreamReader(fileName);
+            string buffer;
+
+            while ((buffer = dataLoad.ReadLine()) != "end_planets")
+            {
+                planets.Add(new Planet(buffer));
+            }
+
+            n = planets.Count;
+            ma = new int[n, n];
+            while ((buffer = dataLoad.ReadLine()) != null)
+            {
+                string[] local = buffer.Split(' ');
+                int i = int.Parse(local[0]);
+                int j = int.Parse(local[1]);
+                int value = int.Parse(local[2]);
+                ma[i, j] = value;
+                ma[j, i] = value;
+            }
+        }
+
+        public static void DrawMap()
+        {
+            for(int i=0; i<n-1; i++)
+                for(int j=i+1; j<n; j++)
+                {
+                    if (ma[i, j] != 0)
+                        grp.DrawLine(new Pen(Color.White, 5), new Point((int)(planets[i].location.X * zoomX), (int)(planets[i].location.Y * zoomY)), 
+                            new Point((int)(planets[j].location.X * zoomX), (int)(planets[j].location.Y * zoomY)));
+                }
+
+            foreach (Planet p in planets)
+            {
+                p.Draw();
+            }
+        }
+
+        public static void RefreshMap()
+        {
+            display.Image = bmp;
+        }
+    }
+}
---
+++ Anul III/Sem II 2019-2020/Tehnici avansate de programare/LicentaDemo/LicentaDemo/Planet.cs
@@ -0,0 +1,31 @@
+﻿using System.Drawing;
+
+namespace LicentaDemo
+{
+    public class Planet
+    {
+        public Point location;
+        public Color fillColor;
+        public int size;
+        public string name;
+        public Player owner;
+
+        public Planet(string data)
+        {
+            string[] t = data.Split(new char[] { '[', ']' });
+            string[] local = t[3].Split(',');
+            location = new Point(int.Parse(local[0]), int.Parse(local[1]));
+            size = int.Parse(local[2]);
+            name = t[5];
+
+            local = t[9].Split(',');
+            fillColor = Color.FromArgb(int.Parse(local[0]), int.Parse(local[1]), int.Parse(local[2]));
+        }
+
+        public void Draw()
+        {
+            Engine.grp.FillEllipse(new SolidBrush(fillColor), (location.X - size) * Engine.zoomX, (location.Y - size) * Engine.zoomY, (2 * size + 1) * Engine.zoomX, (2 * size + 1) * Engine.zoomY);
+            Engine.grp.DrawString(name, new Font("Arial", 20, FontStyle.Bold), new SolidBrush(Color.Green), new Point((int)(location.X * Engine.zoomX), (int)(location.Y * Engine.zoomY)));
+        }
+    }
+}
