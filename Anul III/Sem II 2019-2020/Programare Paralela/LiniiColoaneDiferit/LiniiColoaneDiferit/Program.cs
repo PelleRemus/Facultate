7edd47fdb393f8527feb57c7c6a34bb9294fff91
PelleRemus <remusnicolaepelle@gmail.com>
2020-03-19 23:23:28 +0200

    Curs PP 19.03.2020 - Program pentru compararea a doi vectori in functie de puterea acestora, si "fenomenul" matricei realizata facand operatia xor intre linia si coloana elementului.

---
+++ Anul III/Sem II 2019-2020/Programare Paralela/LiniiColoaneDiferit/LiniiColoaneDiferit/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiniiColoaneDiferit
+{
+    class Program
+    {
+        static int[,] matrix;
+        static int n;
+
+        static void Main(string[] args)
+        {
+            n = 9;
+            matrix = new int[n, n];
+            
+            /*for (int i = 0; i < n; i++)
+                matrix[0, i] = i;
+
+            for (int i = 1; i < n; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    bool[] v = new bool[n*n];
+                    for (int k = 0; k < i; k++)
+                        v[matrix[k, j]] = true;
+                    for (int k = 0; k < j; k++)
+                        v[matrix[i, k]] = true;
+
+                    for (int k = 0; k < n*n; k++)
+                        if (!v[k])
+                        {
+                            matrix[i, j] = k;
+                            break;
+                        }
+                }
+            */
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    matrix[i, j] = i ^ j;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                    Console.Write(matrix[i, j] + " ");
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Anul III/Sem II 2019-2020/Programare Paralela/PutereVector/PutereVector/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PutereVector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c = Compare(new int[] { 1, 1, 1, 1, 1, 1, 1 }, new int[] { 2, 2, 3, 3, 4 });
+            if(c==-1)
+                Console.WriteLine("Primul vector are putere mai mare");
+            else if(c==1)
+                Console.WriteLine("Al doilea vector are putere mai mare");
+            else
+                Console.WriteLine("Vectorii au puteri egale");
+            Console.ReadKey();
+        }
+
+        static int[] Putere(int[] v)
+        {
+            Sort(v);
+            for (int i = 0; i < v.Length; i++)
+                Console.Write(v[i] + " ");
+            Console.WriteLine();
+
+            List<int> c = new List<int>();
+            int n = v.Length;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                int count = 1;
+                while (i < n - 1 && v[i] == v[i + 1])
+                {
+                    count++;
+                    i++;
+                }
+                c.Add(count);
+            }
+            if (v[n - 1] != v[n - 2])
+                c.Add(1);
+
+            int[] toReturn = c.ToArray();
+            Sort(toReturn);
+            return toReturn;
+        }
+
+        static int Compare(int[] v, int[] u)
+        {
+            int[] a = Putere(v);
+            int[] b = Putere(u);
+
+            for (int i = 0; i < Math.Min(a.Length, b.Length); i++)
+            {
+                if (a[i] < b[i])
+                    return 1;
+                if (a[i] > b[i])
+                    return -1;
+            }
+
+            if (a.Length < b.Length)
+                return 1;
+            if (a.Length > b.Length)
+                return -1;
+            return 0;
+        }
+
+        static void Sort(int[] v)
+        {
+            for (int i = 1; i < v.Length; i++)
+                for (int j = i; j > 0; j--)
+                    if (v[j] > v[j - 1])
+                    {
+                        int aux = v[j];
+                        v[j] = v[j - 1];
+                        v[j - 1] = aux;
+                    }
+        }
+    }
+}
