330fdb8cc2f951d0f0a795678203bf2d7a6c8377
PelleRemus <remusnicolaepelle@gmail.com>
2020-04-02 14:30:02 +0300

    Curs PP 02.04.2020 - Clasa numerelor complexe ce poate intra la partea scrisa de la licenta. Exersarea suprascrierii operatorilor pe clasa Vector

---
+++ Anul III/Sem II 2019-2020/Programare Paralela/NrComplexe/NrComplexe/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NrComplexe
+{
+    public class Complex
+    {
+        public float real, imaginar;
+
+        public Complex()
+        {
+            real = float.Parse(Console.ReadLine());
+            imaginar = float.Parse(Console.ReadLine());
+        }
+
+        public Complex(float real, float imaginar)
+        {
+            this.real = real;
+            this.imaginar = imaginar;
+        }
+
+        public float Norma()
+        {
+            return (float)Math.Sqrt(real * real + imaginar * imaginar);
+        }
+
+        public static Complex operator+ (Complex a, Complex b)
+        {
+            float real = a.real + b.real;
+            float imaginar = a.imaginar + b.imaginar;
+            return new Complex(real, imaginar);
+        }
+
+        public static Complex operator*(Complex a, Complex b)
+        {
+            float real = a.real * b.real - a.imaginar * b.imaginar;
+            float imaginar = a.real * b.imaginar + a.imaginar * b.real;
+            return new Complex(real, imaginar);
+        }
+
+        public static bool operator== (Complex a, Complex b)
+        {
+            if (a.real == b.real && a.imaginar == b.imaginar)
+                return true;
+            return false;
+        }
+        public static bool operator!=(Complex a, Complex b)
+        {
+            if (a==b)
+                return false;
+            return true;
+        }
+
+        public static bool operator< (Complex a, Complex b)
+        {
+            if (a.real < b.real)
+                return true;
+            if (a.real > b.real)
+                return false;
+            if (a.imaginar < b.imaginar)
+                return true;
+            return false;
+        }
+        public static bool operator> (Complex a, Complex b)
+        {
+            if (a < b || a == b)
+                return false;
+            return true;
+        }
+
+        public override string ToString()
+        {
+            if (imaginar > 0)
+                return real + "+i" + imaginar;
+            else if (imaginar < 0)
+                return real + "-i" + -imaginar;
+            else
+                return real.ToString();
+        }
+    }
+
+    class Program
+    {
+        static Complex Suma(Complex a, Complex b)
+        {
+            float real = a.real + b.real;
+            float imaginar = a.imaginar + b.imaginar;
+            return new Complex(real, imaginar);
+        }
+
+        static Complex Produs(Complex a, Complex b)
+        {
+            float real = a.real * b.real - a.imaginar * b.imaginar;
+            float imaginar = a.real * b.imaginar + a.imaginar * b.real;
+            return new Complex(real, imaginar);
+        }
+
+        static void Main(string[] args)
+        {
+            Complex a = new Complex(2, 7);
+            Complex b = new Complex();
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(a == b);
+            Console.WriteLine(a.Norma());
+            Complex c = a + b;
+            Complex d = a * b;
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Anul III/Sem II 2019-2020/Programare Paralela/OperatoriVectori/OperatoriVectori/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatoriVectori
+{
+    public class Vector
+    {
+        public int[] value;
+
+        public Vector(int n)
+        {
+            value = new int[n];
+            for (int i = 0; i < n; i++)
+                value[i] = Program.rnd.Next(10);
+        }
+
+        public static bool operator ==(Vector a, Vector b)
+        {
+            if (a.value.Length != b.value.Length)
+                return false;
+            for (int i = 0; i < a.value.Length; i++)
+                if (a.value[i] != b.value[i])
+                    return false;
+            return true;
+        }
+        public static bool operator !=(Vector a, Vector b)
+        {
+            if (a == b)
+                return false;
+            return true;
+        }
+
+        public static bool operator <(Vector a, Vector b)
+        {
+            int n = a.value.Length, m = b.value.Length;
+            for (int i = 0; i < Math.Min(n, m); i++)
+            {
+                if (a.value[i] < b.value[i])
+                    return true;
+                if (a.value[i] > b.value[i])
+                    return false;
+            }
+            if (n < m)
+                return true;
+            return false;
+        }
+
+        public static bool operator >(Vector a, Vector b)
+        {
+            if (a < b || a == b)
+                return false;
+            return true;
+        }
+    }
+
+    class Program
+    {
+        public static Random rnd = new Random();
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
