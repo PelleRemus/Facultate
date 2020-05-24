75332d42a107b43138ea1ad3db08af48a98e3aba
PelleRemus <remusnicolaepelle@gmail.com>
2020-05-24 23:57:23 +0300

    Basics of Parallel programming using arrays and matrices

---
+++ Anul III/Sem II 2019-2020/Programare Paralela/MatriciParalel/MatriciParalel/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatriciParalel
+{
+    class Program
+    {
+        static int[,] matrix;
+        static int n;
+        static Random rnd = new Random();
+
+        static void Main(string[] args)
+        {
+            //initializare si afisare matrice
+            int n = 5;
+            matrix = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = rnd.Next(2);
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            //initializare taskuri, calcularea sumelor in acestea
+            int S = 0;
+            Task<int>[] sume = new Task<int>[n];
+            for (int i = 0; i < n; i++)
+            {
+                int index = i;
+                sume[i] = Task<int>.Factory.StartNew(() =>
+                {
+                    int suma = 0;
+                    for (int j = 0; j < n; j++)
+                        suma += matrix[index, j];
+                    return suma;
+                });
+            }
+
+            //calcularea sumei finale, si afisarea sumelor intermediare
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(sume[i].Result + " ");
+                S += sume[i].Result;
+            }
+            Console.WriteLine();
+            Console.WriteLine(S);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Anul III/Sem II 2019-2020/Programare Paralela/VectoriParalel/VectoriParalel/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectoriParalel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //initializare vector
+            int n = 100, threads = 5;
+            int[] v = new int[n];
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+                v[i] = rnd.Next(1, 10);
+
+            //afisare vector
+            for(int i=0; i<n; i++)
+                Console.Write(v[i]+ " ");
+            Console.WriteLine();
+
+            //initializare taskuri; calculare suma pt fiecare task
+            Task<int>[] tasks = new Task<int>[threads];
+            for (int i = 0; i < threads; i++)
+            {
+                int index = i; //i se modifica in for, trebuie salvat intr-o alta variabila ca altfel sare peste elemente
+                tasks[i] = Task<int>.Factory.StartNew(() =>
+                {
+                    int s = 0;
+                    for (int j = index * n / threads; j < (index + 1) * n / threads; j++)
+                        s += v[j];
+                    return s;
+                });
+            }
+
+            //calculare suma in modul normal
+            int suma = 0;
+            for (int i = 0; i < n; i++)
+                suma += v[i];
+            Console.WriteLine("Suma calculata normal: " + suma);
+
+            //calculare suma paralel
+            suma = 0;
+            for (int i = 0; i < threads; i++)
+                suma += tasks[i].Result;
+            Console.WriteLine("Suma calculata paralel: " + suma);
+            Console.ReadKey();
+        }
+    }
+}
